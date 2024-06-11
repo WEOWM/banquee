a9141e2e48c94a16dc9158a8cf615990db168d15
WMOWM <basims0077@gmail.com>
2024-06-11 15:52:40 +0530

    mobile update

---
+++ src/Component/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className="loading">
+            <div className="spinner "></div>
+            <p className='ps-3'>Loading...  </p>
+        </div>
+    )
+}
+
+export default Loading
